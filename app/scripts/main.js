6909e136ebb63d70b483bd49528b87f5feda003e
Vidal Graupera <vgraupera@gmail.com>
2015-01-21 00:06:21 -0800

    jshint fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,11 @@
-/* jshint devel:true */
-$(function(){
-  var clock = document.getElementById("countdown-holder")
-    , targetDate = new Date(2015, 04, 02, 18, 24);
+/* jshint devel:true, strict:false, laxcomma:true, bitwise:false */
+/* global countdown */
 
-  var units = countdown.MONTHS | countdown.DAYS | countdown.HOURS | countdown.MINUTES;
+$(function(){
+  var clock = $('countdown-holder')
+    , targetDate = new Date(2015, 04, 02, 18, 24)
+    , units = (countdown.MONTHS | countdown.DAYS | countdown.HOURS | countdown.MINUTES);
+
   clock.innerHTML = countdown(targetDate, null, units).toString();
   setInterval(function(){
     clock.innerHTML = countdown(targetDate, null, units).toString();
