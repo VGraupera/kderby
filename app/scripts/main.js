abf7cba453c2442049888dfba6137c3ddc93d066
Vidal Graupera <vgraupera@gmail.com>
2015-01-20 23:38:12 -0800

    add countdown timer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,10 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+$(function(){
+  var clock = document.getElementById("countdown-holder")
+    , targetDate = new Date(2015, 04, 02, 18, 24);
+
+  var units = countdown.MONTHS | countdown.DAYS | countdown.HOURS | countdown.MINUTES;
+  clock.innerHTML = countdown(targetDate, null, units).toString();
+  setInterval(function(){
+    clock.innerHTML = countdown(targetDate, null, units).toString();
+  }, 1000);});
