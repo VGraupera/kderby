dc85f6a0e2c754c56e8d180fe1efd95e3125c7b6
Vidal Graupera <vgraupera@gmail.com>
2015-01-24 21:40:29 -0800

    add order form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,2 @@
-/* jshint devel:true, strict:false, laxcomma:true, bitwise:false */
-/* global countdown */
+/* jshint devel:true */
 
-$(function(){
-  var clock = $('countdown-holder')
-    , targetDate = new Date(2015, 04, 02, 18, 24)
-    , units = (countdown.MONTHS | countdown.DAYS | countdown.HOURS | countdown.MINUTES);
-
-  clock.innerHTML = countdown(targetDate, null, units).toString();
-  setInterval(function(){
-    clock.innerHTML = countdown(targetDate, null, units).toString();
-  }, 1000);});
